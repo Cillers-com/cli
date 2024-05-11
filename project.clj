812727fc4421924f13522dc869faebf735eb65e6
Peder Linder <pederbl@gmail.com>
2024-05-11 07:07:10 +0200

    Release v0.0.6

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject cli "0.0.4"
+(defproject cli "0.0.6"
   :description "The Cillers CLI: For creation, devlopment and management of Cillers systems"
   :url "https://cillers.com"
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot cli.core
   :target-path "target"
   :profiles {:uberjar {:aot :all
+                       :dependencies [[com.github.clj-easy/graal-build-time "1.0.5"]]
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+
